f21fb5743eb6fe88c8988385ee1749f05231841c
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-07-29 16:01:09 +0530

    implemented filters feature

--- frontend/src/app/home/filters/filters.component.ts
+++ frontend/src/app/home/filters/filters.component.ts
@@ -1,16 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { assets, JobCategories, JobLocations } from '../../../assets/assets';
 import { CommonModule } from '@angular/common';
+import { emit } from 'process';
 
 @Component({
   selector: 'app-filters',
   imports: [CommonModule, FormsModule],
   templateUrl: './filters.component.html',
-  styleUrl: './filters.component.css'
+  styleUrl: './filters.component.css',
 })
-
 export class FiltersComponent {
   jobCategories = JobCategories;
   jobLocations = JobLocations;
+
+  filterCategories: string[] = [];
+  locationCategories: string[] = [];
+
+  @Output() filteredChanges = new EventEmitter<{
+    categories: string[];
+    location: string[];
+  }>();
+
+  emitFilters() {
+    this.filteredChanges.emit({
+      categories: this.filterCategories,
+      location: this.locationCategories,
+    });
+  }
+
+  onCategoryChange(event: Event, category: string) {
+    const checked = (event?.target as HTMLInputElement).checked;
+    if (checked) {
+      this.filterCategories.push(category);
+    } else {
+      this.filterCategories = this.filterCategories.filter(
+        (c) => c != category
+      );
+    }
+    this.emitFilters();
+  }
+
+  onLocationChange(event: Event, location: string) {
+    const checked = (event?.target as HTMLInputElement).checked;
+    if (checked) {
+      this.locationCategories.push(location);
+    } else {
+      this.locationCategories = this.locationCategories.filter(
+        (c) => c != location
+      );
+    }
+    this.emitFilters();
+  }
 }
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { FiltersComponent } from './filters/filters.component';
 import { LatestJobsComponent } from './latest-jobs/latest-jobs.component';
 import { AppDowloadComponent } from './app-dowload/app-dowload.component';
 import { assets, jobsData } from '../../assets/assets';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-home',
   imports: [
@@ -27,5 +28,20 @@ export class HomeComponent {
         job.title.toLowerCase().includes(title) &&
         job.location.toLowerCase().includes(location)
     );
+  }
+
+  handleFilteredChanges(event: { categories: string[]; location: string[] }) {
+    const lowerCaseCategories = event.categories;
+    const lowerCaseLocations = event.location;
+
+    this.filteredJobs = this.allJobs.filter((job) => {
+      const categoryMatch =
+        lowerCaseCategories.length == 0 ||
+        lowerCaseCategories.includes(job.category);
+      const locationMatch =
+        lowerCaseLocations.length == 0 ||
+        lowerCaseLocations.includes(job.location);
+      return categoryMatch && locationMatch;
+    });
   }
 }
