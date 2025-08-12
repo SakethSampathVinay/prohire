fcc3c41204092c6ba7ca967a085756356e00be20
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-08-12 18:51:41 +0530

    changed environment link

--- admin/src/app/services/auth.service.ts
+++ admin/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Token } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
